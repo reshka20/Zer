04f8ff2bac4d088d9e7daa1bb26b779d410ac902
Resad <resad547294@gmail.com>
2021-03-12 03:19:25 +0400

    zer Atmaq

---
+++ app.js
@@ -0,0 +1,42 @@
+function ZERAT (){
+    var birincizer = Math.ceil(Math.random() * 6);
+    var ikincizer = Math.ceil(Math.random() * 6);
+    if(birincizer==1){
+            document.getElementById("birincizer").src = "img/01.png";
+    }
+    else if(birincizer==2){
+        document.getElementById("birincizer").src = "img/02.png";
+}
+else if(birincizer==3){
+    document.getElementById("birincizer").src = "img/03.png";
+}
+else if(birincizer==4){
+    document.getElementById("birincizer").src = "img/04.png";
+}
+else if(birincizer==5){
+    document.getElementById("birincizer").src = "img/05.png";
+}
+else if(birincizer==6){
+    document.getElementById("birincizer").src = "img/06.png";
+}
+if(ikincizer==1){
+    document.getElementById("ikincizer").src = "img/01.png";
+}
+else if(ikincizer==2){
+document.getElementById("ikincizer").src = "img/02.png";
+}
+else if(ikincizer==3){
+document.getElementById("ikincizer").src = "img/03.png";
+}
+else if(ikincizer==4){
+document.getElementById("ikincizer").src = "img/04.png";
+}
+else if(ikincizer==5){
+document.getElementById("ikincizer").src = "img/05.png";
+}
+else if(ikincizer==6){
+document.getElementById("ikincizer").src = "img/06.png";
+}
+
+}
+
